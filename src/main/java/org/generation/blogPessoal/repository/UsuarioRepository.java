f57be79511d3d4a62091c4b56d8615a3ca8b5883
juliasobral <juliasobralm1@gmail.com>
2021-11-11 10:47:36 -0300

    [add .repository]

--- src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -1,5 +1,12 @@
 package org.generation.blogPessoal.repository;
 
-public interface UsuarioRepository {
+import java.util.Optional;
 
+import org.generation.blogPessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository  extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findAllByUsuario (String usuario);
+	
 }
